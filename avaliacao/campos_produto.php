007fc41aa64134d8a9c7edc703ae178e7c65fe76
elisanna <elisanna.nana@yahoo.com>
2014-01-10 12:12:43 -0200

    Create campos_produto.php

---
+++ avaliacao/campos_produto.php
@@ -0,0 +1,20 @@
+        <label>Produto: <br/></label><input type="text" name="nomeProduto" />
+              <br/><br/>
+            <input type="checkbox" value="sim" name="produtoNovo" checked="checked"/>
+              <label> Produto Novo</label>
+              <br/><br/>
+              <fieldset><label>Prazo:</label>
+              <select name="prazo">
+                <option>5 Dias </option>
+                <option>10 Dias </option>
+                <option>15 Dias </option>
+              </fieldset></select>
+              <br/><br/>
+                <label>Cor do Produto:</label> <br/>
+                <input type="radio" name="cor" value="Vermelho" /><label>Vermelho</label>
+                <input type="radio" name="cor" value="Cinza" /><label>Cinza</label>
+                <input type="radio" name="cor" value="Azul" /><label> Azul</label>
+                <input type="radio" name="cor" value="Preto" /><label> Preto</label>
+              <br/><br/>
+                <label>Detalhes do Produto </label><br/>
+                <textarea name="detalhes" rows="8" cols="50"></textarea> 
