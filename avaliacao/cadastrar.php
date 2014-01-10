9651b8cc3633a73810df0a4ba745c2d1af080026
elisanna <elisanna.nana@yahoo.com>
2014-01-10 12:08:26 -0200

    Create cadastrar.php

---
+++ avaliacao/cadastrar.php
@@ -0,0 +1,21 @@
+<?php
+    require_once("menu.php");
+    
+    session_start();
+    
+    $nomeProduto = $_REQUEST["nomeProduto"];
+    $produtoNovo = $_REQUEST["produtoNovo"];
+    $cor = $_REQUEST["cor"];
+    $prazo = $_REQUEST["prazo"];
+    $detalhes = $_REQUEST["detalhes"];
+    
+    
+    if(!isset($_SESSION["cadastroProdutos"])) {
+        $_SESSION["cadastroProdutos"] = array();
+        
+        $produto = array("nomeProduto" => $nomeProduto, "produtoNovo" => $produtoNovo, "cor" => $cor, "prazo" => $prazo, "detalhes" => $detalhes);
+        
+        array_push($_SESSION["cadastroProdutos"], $produto);
+        
+        echo "Cadastro realizado com sucesso!";
+    }
